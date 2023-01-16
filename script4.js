51becd7ecee7990e17f6a2e8b6d039bec7f354ff
darterss <darters@mail.ru>
2023-01-17 00:26:08 +0300

    script4 optimized

--- script4.js
+++ script4.js
@@ -1,11 +1,9 @@
 let number1 = 10;
 let number2 = 17;
-let numbers = number2 - number1 + 2;
 
 function print (){
     console.log(number1);
+    if (number1 === number2) clearInterval (intervalID);
     number1++;
 }
 const intervalID = setInterval (print, 1000);
-const clear = () => clearInterval (intervalID);
-setTimeout (clear, numbers*1000);
